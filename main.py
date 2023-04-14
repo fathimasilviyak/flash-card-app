2b478ddde121cf073447b97ae1e42fc3320dfce5
fathimasilviyak <silviyakvm@gmail.com>
2023-04-14 14:07:26 +0530

    remove known words from data, and show only unknown words

--- main.py
+++ main.py
@@ -3,10 +3,17 @@ from tkinter import *
 import pandas
 import random
 
-BACKGROUND_COLOR = "#B1DDC6"
-data = pandas.read_csv("data/french_words.csv")
-data_dict = data.to_dict(orient="records")
 current_card = {}
+data_dict = {}
+
+BACKGROUND_COLOR = "#B1DDC6"
+try:
+    data = pandas.read_csv("data/words_to_learn.csv")
+except FileNotFoundError:
+    original_data = pandas.read_csv("data/french_words.csv")
+    data_dict = original_data.to_dict(orient="records")
+else:
+    data_dict = data.to_dict(orient="records")
 
 
 def next_card():
@@ -25,6 +32,13 @@ def flip_card():
     canvas.itemconfig(card_image, image=back_image)
 
 
+def is_known():
+    data_dict.remove(current_card)
+    data_f = pandas.DataFrame(data_dict)
+    data_f.to_csv("data/words_to_learn.csv", index=False)
+    next_card()
+
+
 window = Tk()
 window.title("Flash Card App")
 window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
@@ -39,10 +53,10 @@ canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
 canvas.grid(row=0, column=0, columnspan=2)
 right_button_img = PhotoImage(file="images/right.png")
 left_button_img = PhotoImage(file="images/wrong.png")
-right_button = Button(image=right_button_img, highlightthickness=0, command=next_card)
-right_button.grid(row=1, column=0)
 left_button = Button(image=left_button_img, highlightthickness=0, command=next_card)
-left_button.grid(row=1, column=1)
+left_button.grid(row=1, column=0)
+right_button = Button(image=right_button_img, highlightthickness=0, command=is_known)
+right_button.grid(row=1, column=1)
 
 next_card()
 
