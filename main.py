061f314a7cd809e91660fbe637797d3d6be51720
fathimasilviyak <silviyakvm@gmail.com>
2023-04-14 10:56:46 +0530

    every 3 seconds flip the card and show the other side

--- main.py
+++ main.py
@@ -6,21 +6,33 @@ import random
 BACKGROUND_COLOR = "#B1DDC6"
 data = pandas.read_csv("data/french_words.csv")
 data_dict = data.to_dict(orient="records")
+current_card = {}
 
 
 def next_card():
+    global current_card, flip_timer
+    window.after_cancel(flip_timer)
     current_card = random.choice(data_dict)
-    canvas.itemconfig(card_title, text="French")
-    canvas.itemconfig(card_word, text=current_card['French'])
+    canvas.itemconfig(card_title, text="French", fill="black")
+    canvas.itemconfig(card_word, text=current_card['French'], fill="black")
+    canvas.itemconfig(card_image, image=front_image)
+    flip_timer = window.after(3000, flip_card)
+
+
+def flip_card():
+    canvas.itemconfig(card_title, text="English", fill="white")
+    canvas.itemconfig(card_word, text=current_card['English'], fill="white")
+    canvas.itemconfig(card_image, image=back_image)
 
 
 window = Tk()
 window.title("Flash Card App")
 window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
-
+flip_timer = window.after(3000, flip_card)
 canvas = Canvas(width=800, height=526)
 front_image = PhotoImage(file="images/card_front.png")
-canvas.create_image(400, 263, image=front_image)
+back_image = PhotoImage(file="images/card_back.png")
+card_image = canvas.create_image(400, 263, image=front_image)
 card_title = canvas.create_text(400, 150, text="", font=("arial", 40, "italic"))
 card_word = canvas.create_text(400, 263, text="", font=("arial", 60, "bold"))
 canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
